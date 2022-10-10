929fd03aa3d5e9ef6a6e32b49431ced51352be2c
Arthur <arthuroaraujo@gmail.com>
2022-10-10 17:33:39 -0300

    Req9

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,4 +1,4 @@
-import { LOGIN } from '../actions';
+import { LOGIN, ASSERTIONS, SCORE } from '../actions';
 
 const INICIAL_STATE = {
   name: '',
@@ -14,6 +14,16 @@ const player = (state = INICIAL_STATE, action) => {
       ...state,
       gravatarEmail: action.payload.email,
       name: action.payload.name,
+    };
+  case ASSERTIONS:
+    return {
+      ...state,
+      assertions: action.payload,
+    };
+  case SCORE:
+    return {
+      ...state,
+      score: state.score + action.payload,
     };
   default:
     return state;
